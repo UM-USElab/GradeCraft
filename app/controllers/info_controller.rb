8b627841e97343fbc2897b20f58d82faa1274ab8
Cory Kaufman-Schofield <cory@corykaufman.com>
2011-12-16 16:31:30 -0500

    Added info controller

---
+++ app/controllers/info_controller.rb
@@ -0,0 +1,7 @@
+class InfoController < ApplicationController
+
+  def index
+    render :text => 'Hello world!'
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Grader::Application.routes.draw do
+  root :to => 'info#index'
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
