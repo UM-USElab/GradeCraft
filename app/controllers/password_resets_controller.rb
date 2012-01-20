011d0dcdebc760b6e7667e6bfca9d772f2592f89
Cory Kaufman-Schofield <cory@corykaufman.com>
2012-01-19 23:00:46 -0500

    Added password reset mailer

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -1,26 +1,26 @@
 class PasswordResetsController < ApplicationController
   skip_before_filter :require_login
-    
+
   # request password reset.
   # you get here when the user entered his email in the reset password form and submitted it.
-  def create 
+  def create
     @user = User.find_by_email(params[:email])
-        
+
     # This line sends an email to the user with instructions on how to reset their password (a url with a random token)
     @user.deliver_reset_password_instructions! if @user
-        
+
     # Tell the user instructions have been sent whether or not email was found.
     # This is to not leak information to attackers about which emails exist in the system.
     redirect_to(root_path, :notice => 'Instructions have been sent to your email.')
   end
-    
+
   # This is the reset password form.
   def edit
     @user = User.load_from_reset_password_token(params[:id])
     @token = params[:id]
     not_authenticated unless @user
   end
-      
+
   # This action fires when the user has sent the reset password form.
   def update
     @token = params[:token]
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -13,8 +13,13 @@ Grader::Application.configure do
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
 
-  # Don't care if the mailer can't send
-  config.action_mailer.raise_delivery_errors = false
+  ### ActionMailer Config
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  # A dummy setup for development - no deliveries, but logged
+  config.action_mailer.delivery_method = :test
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default :charset => "utf-8"
 
   # Print deprecation notices to the Rails logger
   config.active_support.deprecation = :log
