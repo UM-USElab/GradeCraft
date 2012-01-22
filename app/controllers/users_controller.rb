3b907cb664efc9fe7b22013e5c0b6f535d9f2952
Cait Holman <caitlin.holman@gmail.com>
2012-01-22 17:55:59 -0500

    added action files

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,72 +1,43 @@
 class UsersController < ApplicationController
-  # GET /users
-  # GET /users.json
+  respond_to :html, :json
 
   skip_before_filter :require_login, :only=>[:create,:new]
   before_filter :'ensure_staff?', :only=>[:index,:destroy]
 
   def index
     @title = "View all Users"
-    @users = User.all
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.json { render json: @users }
+    @role = params[:role]
+    if @role
+      @users = User.where(:role => @role)
+    else
+      @users = User.all
     end
+    respond_with @users
   end
 
-  # GET /users/1
-  # GET /users/1.json
   def show
     @title = "View User"
-    @user = User.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.json { render json: @user }
-    end
+    respond_with @user = User.find(params[:id])
   end
 
-  # GET /users/new
-  # GET /users/new.json
   def new
     @title = "Register"
-    @user = User.new
     @teams = Team.all
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.json { render json: @user }
-    end
+    respond_with @user = User.new
   end
 
-  # GET /users/1/edit
   def edit
     @title = "Edit User"
-    @user = User.find(params[:id])
     @teams = Team.all
+    respond_with @user = User.find(params[:id])
   end
 
-  # POST /users
-  # POST /users.json
   def create
-    @user = User.new(params[:user])
-    @user = User.create(params[:user])
     @teams = Team.all
-
-    respond_to do |format|
-      if @user.save
-        format.html { redirect_to(:users, :notice => 'User was successfully created.') }
-        format.json { render json: @user, status: :created, location: @user }
-      else
-        format.html { render action: "new" }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
-    end
+    @user = User.create(params[:user])
+    respond_with @user
   end
 
-  # PUT /users/1
-  # PUT /users/1.json
   def update
     @user = User.find(params[:id])
     @teams = Team.all
@@ -81,26 +52,24 @@ class UsersController < ApplicationController
         format.json { render json: @user.errors, status: :unprocessable_entity }
       end
     end
+    
+    @user.update_attributes(params[:user])
+    respond_with @user
   end
 
-  # DELETE /users/1
-  # DELETE /users/1.json
   def destroy
     @user = User.find(params[:id])
     @user.destroy
+    respond_with @user
+  end
 
-    respond_to do |format|
-      format.html { redirect_to users_url }
-      format.json { head :ok }
-    end
-  end
-  
   def edit_profile
-    @user = current_user
+    respond_with @user = current_user
   end
-  
+
   def update_profile
     @user = current_user
     @user.update_attribute(:password, params[:password]) if params[:password] == params[:confirm_password]
+    respond_with @user
   end
 end
