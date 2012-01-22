4a6110db0dfb883236f353e29a46ef1b62ebbe4f
Cory Kaufman-Schofield <cory@corykaufman.com>
2012-01-22 16:58:09 -0500

    Made before filter actually work

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,15 +1,15 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  
+
   before_filter :require_login, :except => [:not_authenticated]
-  
+
   protected
   def not_authenticated
     redirect_to root_path, :alert => "Please login first."
-  end      
-  
+  end
+
   def ensure_staff?
-    current_user.is_staff?
+    return not_authenticated unless current_user.is_staff?
   end
 
 end
