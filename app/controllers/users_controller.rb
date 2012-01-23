765c12ceb766acbe6b6c491e9b83a1acb00e05ac
Cait Holman <caitlin.holman@gmail.com>
2012-01-22 22:40:40 -0500

    fixed update user roles

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -47,14 +47,10 @@ class UsersController < ApplicationController
         format.html { redirect_to @user, notice: 'User was successfully updated.' }
         format.json { head :ok }
       else
-        debugger
         format.html { render action: "edit" }
         format.json { render json: @user.errors, status: :unprocessable_entity }
       end
     end
-    
-    @user.update_attributes(params[:user])
-    respond_with @user
   end
 
   def destroy
