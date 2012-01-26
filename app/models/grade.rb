92e3a462102a07a1ba569709578349c94ca5d6d1
Cait Holman <caitlin.holman@gmail.com>
2012-01-25 23:55:50 -0500

    Validated grade submissions

--- app/models/grade.rb
+++ app/models/grade.rb
@@ -5,5 +5,8 @@ class Grade < ActiveRecord::Base
   has_many :badges, :through => :earned_badges
   
   attr_accessible :feedback
-
+  
+  validates :user_id, :presence => true
+  validates :score, :presence => true
+  validates :assignment_id, :presence => true
 end
