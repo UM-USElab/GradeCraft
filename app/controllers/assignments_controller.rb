0f5293b2ecc3643441f53d2e59b852e3223fa9cf
Cory Kaufman-Schofield <cory@corykaufman.com>
2012-01-27 04:07:46 -0500

    Added partials for assignment form

--- app/controllers/assignments_controller.rb
+++ app/controllers/assignments_controller.rb
@@ -1,89 +1,49 @@
 class AssignmentsController < ApplicationController
+  respond_to :html, :json
 
-  before_filter :ensure_staff?, :only => [:new,:edit,:create,:update,:destroy] 
+  before_filter :ensure_staff?, :except => [:index]
 
-  # GET /assignments
-  # GET /assignments.json
   def index
-    @title = "View all Assignments" 
-    @assignments = Assignment.all
-    respond_to do |format|
-      format.html # index.html.erb
-      format.json { render json: @assignments }
-    end
+    @title = "View all Assignments"
+    respond_with @assignments = Assignment.all
   end
 
-  # GET /assignments/1
-  # GET /assignments/1.json
   def show
     @title = "View all Assignments"
-    @assignment = Assignment.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.json { render json: @assignment }
-    end
+    respond_with @assignment = Assignment.find(params[:id])
   end
 
-  # GET /assignments/new
-  # GET /assignments/new.json
   def new
     @title = "Create a New Assignment"
-    @assignment = Assignment.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.json { render json: @assignment }
-    end
+    klass = %w{Attendance Blogging ReadingRection}.include?(params[:type]) || Assignment
+    respond_with @assignment = klass.new
   end
 
-  # GET /assignments/1/edit
   def edit
     @title = "Edit this Assignment"
-    @assignment = Assignment.find(params[:id])
+    respond_with @assignment = Assignment.find(params[:id])
   end
 
-  # POST /assignments
-  # POST /assignments.json
   def create
     @assignment = Assignment.new(params[:assignment])
-
-    respond_to do |format|
-      if @assignment.save
-        format.html { redirect_to @assignment, notice: 'Assignment was successfully created.' }
-        format.json { render json: @assignment, status: :created, location: @assignment }
-      else
-        format.html { render action: "new" }
-        format.json { render json: @assignment.errors, status: :unprocessable_entity }
-      end
+    if @assignment.save
+      respond_with @assignment, :location => assignment_path(@assignment), :notice => 'Assignment was successfully created.'
+    else
+      respond_with @assignment
     end
   end
 
-  # PUT /assignments/1
-  # PUT /assignments/1.json
   def update
     @assignment = Assignment.find(params[:id])
-
-    respond_to do |format|
-      if @assignment.update_attributes(params[:assignment])
-        format.html { redirect_to @assignment, notice: 'Assignment was successfully updated.' }
-        format.json { head :ok }
-      else
-        format.html { render action: "edit" }
-        format.json { render json: @assignment.errors, status: :unprocessable_entity }
-      end
+    if @assignment.update_attributes(params[:assignment])
+      respond_with @assignment, :location => assignment_path(@assignment), :notice => 'Assignment was successfully updated.'
+    else
+      respond_with @assignment
     end
   end
 
-  # DELETE /assignments/1
-  # DELETE /assignments/1.json
   def destroy
     @assignment = Assignment.find(params[:id])
-    @assignment.destroy
-
-    respond_to do |format|
-      format.html { redirect_to assignments_url }
-      format.json { head :ok }
-    end
+    respond_with @assignment.destroy
   end
 end
