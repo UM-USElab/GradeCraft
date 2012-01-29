276ec2b18ce8e1dad24b6145942cdfbed1cef061
Cait Holman <caitlin.holman@gmail.com>
2012-01-29 16:35:54 -0500

    Updated Assignment Index View

--- app/models/assignment.rb
+++ app/models/assignment.rb
@@ -1,6 +1,8 @@
 class Assignment < ActiveRecord::Base
   has_many :grades
   accepts_nested_attributes_for :grades
+  
+  attr_accessible :title, :description, :point_total, :type, :due_date
 
   scope :one_time, where(:type => nil)
 
