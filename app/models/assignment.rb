fbf8e875aeb32f0c062b5b31b826f668e26e99e0
Cait Holman <caitlin.holman@gmail.com>
2012-02-01 21:39:54 -0500

    Fixed grade permissions.

--- app/models/assignment.rb
+++ app/models/assignment.rb
@@ -1,8 +1,6 @@
 class Assignment < ActiveRecord::Base
   has_many :grades
   accepts_nested_attributes_for :grades
-  
-  attr_accessible :type, :title, :description, :point_total, :due_date
 
   def mass_gradeable?
     true
