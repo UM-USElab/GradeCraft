0a910e8d583b848ddf0029ee0bfb4e507626c1a8
Cait Holman <caitlin.holman@gmail.com>
2012-02-02 00:38:24 -0500

    Added ability to add points and badges outside of assignments

--- app/controllers/info_controller.rb
+++ app/controllers/info_controller.rb
@@ -1,8 +1,15 @@
 class InfoController < ApplicationController
   respond_to :html
 
+  def index
+    @users = User.all
+    @teams = Team.all
+  end
+
   def show
     @title = "Site Credits"
+    @users = User.all
+    @teams = Team.all
   end
 
   def dashboard
