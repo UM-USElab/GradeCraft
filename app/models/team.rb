4b6ef51519f05438ed346ea074205a150428a0d0
Cait Holman <caitlin.holman@gmail.com>
2012-02-03 22:58:09 -0500

    Updated Team Score methods

--- app/models/team.rb
+++ app/models/team.rb
@@ -7,10 +7,14 @@ class Team < ActiveRecord::Base
   def ranking
     
   end
+  
+  def rr_score
+    500 * users.map { |u| u.grades.reading_reactions.map { |g| 1 if g.semis? }.count }.inject(&:+)
+  end
 
   #Overriding the save function so as to update the score every time a grade gets saved
   def calculate_score
-    self.score = (challenge_scores.map(&:score).inject(&:+))
+    score = (challenge_scores.map(&:score).inject(&:+)) + rr_score
   end
   
   
