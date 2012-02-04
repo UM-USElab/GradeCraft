87c1689272013f4c487ae082ece09c64d4274b83
Cory Kaufman-Schofield <cory@corykaufman.com>
2012-02-04 12:28:21 -0500

    Optimized Team#rr_score

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,21 +1,25 @@
 class Team < ActiveRecord::Base
   has_many :users
   has_many :challenge_scores
-  
+
   before_save :calculate_score
-  
-  def ranking
-    
+
+  def user_grades
+    Grade.where(:user_id => users)
   end
-  
+
+  def ranking
+
+  end
+
   def rr_score
-    self.score = 500 * users.map { |u| u.grades.reading_reactions.map { |g| 1 if g.semis? }.count }.inject(&:+)
+    self.score = 500 * user_grades.reading_reactions.where(:semis => 1).count
   end
 
   #Overriding the save function so as to update the score every time a grade gets saved
   def calculate_score
     self.score = (challenge_scores.map(&:score).inject(&:+)) + rr_score
   end
-  
-  
+
+
 end
