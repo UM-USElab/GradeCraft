8ee101cb73d456e24e1e040c2631cf0d5154a8a4
Scott Tsuchiyama <scott.tsuchiyama@gmail.com>
2012-02-05 13:29:48 -0500

    progress bar toggle on student dashboard

--- app/assets/javascripts/new.js
+++ app/assets/javascripts/new.js
@@ -17,28 +17,28 @@ $(document).ready(function(){
 	$(firstpsa).show();
 	
 // Progress bars to student dashboard
-/*
+
 	$('#scoreTotal').hide();
 	$('#userBarTotal').hide();
 	$('#showPossiblePts').hide();
-*/	
+		
 	// ***Change this from hard-coded value when 'possible points' function is written 
 	var availPoints = 300000;
 	
 	var currentUserScore = $('#userScoreNum').html();
 	var totalInProgress = (currentUserScore/availPoints)*100;
 	var totalFull = (currentUserScore/1245000)*100;
-/*	
+	
 	$('#userBarInProgress').progressbar({
 		value:totalInProgress
 	});
-*/	
+	
 	$('#userBarTotal').progressbar({
 		value:totalFull
 	});
-/*	
+	
 	$('#scoreToggle').click(function(){
 		$("#userScoreSection .toggle").toggle();
 	});
-*/	
+	
 });
