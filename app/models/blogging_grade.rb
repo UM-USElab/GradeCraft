9aabc1c1a1fb03c71f4a8fb075157059044cc888
Cait Holman <caitlin.holman@gmail.com>
2012-02-22 10:25:14 -0500

    Workaround for posting blog grades

--- app/models/blogging_grade.rb
+++ app/models/blogging_grade.rb
@@ -1,11 +1,11 @@
 class BloggingGrade < Grade
 
-  #def score
-    #if substantial?
-    #score = 5000
-    #else
-    #score = 1000
-    #end
-  #end
+  def score
+    if substantial?
+    score = 5000
+    else
+    score = 1000
+    end
+  end
 
 end
