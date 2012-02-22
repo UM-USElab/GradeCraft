d6ce1a9ca5af4ce7a3864b1ae9169db0e399bdf5
Cait Holman <caitlin.holman@gmail.com>
2012-02-22 08:08:39 -0500

    Removed attr_accessible temporarily

--- app/models/assignment.rb
+++ app/models/assignment.rb
@@ -2,9 +2,6 @@ class Assignment < ActiveRecord::Base
   has_many :grades, :dependent => :destroy
   accepts_nested_attributes_for :grades
   
-  attr_accessible :title, :description, :point_total, :due_date, :created_at, :updated_at, :level, :type, :present
-  attr_accessible :role, :as => :admin
-  
   default_scope :order => 'due_date ASC'
 
   def mass_gradeable?
