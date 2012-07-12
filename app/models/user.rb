9c1dfcdd469252ba6d8bc23d382dd5431fe07fb7
Cory Kaufman-Schofield <cory@corykaufman.com>
2012-07-12 14:47:11 -0600

    Fixed login form issues

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ActiveRecord::Base
 
   Roles = %w{student professor gsi admin}
   
+  attr_accessor :remember_me
   attr_accessible :username, :email, :crypted_password, :remember_me_token, :avatar_file_name, :role, :team_id, :first_name, :last_name, :sortable_score, :rank, :course_ids, :user_id, :display_name, :private_display, :default_course_id
 
   has_attached_file :avatar,
