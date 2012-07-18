1a1bccea91686877579f66ccd6d144f0c981fcee
Cait Holman <caitlin.holman@gmail.com>
2012-07-17 22:03:16 -0400

    Removed migration file causing an error on Heroku

--- db/migrate/20120629021206_add_badgesto_course.rb
+++
@@ -1,9 +0,0 @@
-class AddBadgestoCourse < ActiveRecord::Migration
-  def up
-    add_column :courses, :badges, :boolean, :default => 1
-  end
-
-  def down
-    add_column :courses, :badges
-  end
-end
