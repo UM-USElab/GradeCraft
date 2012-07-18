2bdde2b3b49094555fbceb0edbedeb1a7cdbf021
Cait Holman <caitlin.holman@gmail.com>
2012-07-17 22:23:14 -0400

    Updated migration file

--- db/migrate/20120629021206_add_badgesto_course.rb
+++ db/migrate/20120629021206_add_badgesto_course.rb
@@ -1,6 +1,6 @@
 class AddBadgestoCourse < ActiveRecord::Migration
   def up
-    add_column :courses, :badges, :boolean
+    add_column :courses, :badges, :boolean, :default => true
   end
 
   def down
