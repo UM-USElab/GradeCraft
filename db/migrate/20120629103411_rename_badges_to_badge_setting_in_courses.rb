67515f4cd30e19a0ee73d35e90eef1b3a6f63068
Cait Holman <caitlin.holman@gmail.com>
2012-07-27 23:41:38 -0400

    Removed faulty migrations

--- db/migrate/20120629103411_rename_badges_to_badge_setting_in_courses.rb
+++ db/migrate/20120629103411_rename_badges_to_badge_setting_in_courses.rb
@@ -1,6 +1,6 @@
 class RenameBadgesToBadgeSettingInCourses < ActiveRecord::Migration
   def change  
-    rename_column :courses, :badges, :badge_setting, :integer
+    rename_column :courses, :badges, :badge_setting
   end
 
 end
