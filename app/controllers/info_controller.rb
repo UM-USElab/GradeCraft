81e24a8bbc4ad934702e03b7994b8bb1550b7955
Cory Kaufman-Schofield <cory@corykaufman.com>
2012-08-22 15:19:42 -0400

    Spit out user/view into partial

--- app/controllers/info_controller.rb
+++ app/controllers/info_controller.rb
@@ -21,6 +21,7 @@ class InfoController < ApplicationController
     @students = current_course.users.students
     @top_ten_students = @students.order('sortable_score DESC').limit(10)
     @bottom_ten_students = @students.order('sortable_score ASC').limit(10)
+    @assignments = current_course.assignments
   end
 
 end
