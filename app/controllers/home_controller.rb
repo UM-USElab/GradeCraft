1caaf744ec87cfbe0ff6b4adea4b60bd46c759f4
Cait Holman <caitlin.holman@gmail.com>
2012-08-31 19:14:52 -0400

    Rendered dashboard on homepage

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,6 +3,13 @@ class HomeController < ApplicationController
   before_filter :require_login, :only => [:login, :register]
   
   def index 
+    @title = "My Dashboard"
+    @teams = current_course.teams.all
+    @students = current_course.users.students
+    @top_ten_students = @students.order('sortable_score DESC').limit(10)
+    @bottom_ten_students = @students.order('sortable_score ASC').limit(10)
+    @assignments = current_course.assignments
+
 
   end
 
