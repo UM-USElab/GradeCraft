a3bd15126a30491b0622c08476706cbd7475524d
Cait Holman <caitlin.holman@gmail.com>
2012-09-24 02:44:22 -0400

    Fixed the home controller missing badge statement

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -12,6 +12,7 @@ class HomeController < ApplicationController
       end
       @teams = current_course.try(:teams)
       @users = current_course.try(:users)
+      @badges = current_course.badges
       @students = @users.students
       @top_ten_students = @students.order('sortable_score DESC').limit(10)
       @bottom_ten_students = @students.order('sortable_score ASC').limit(10)
