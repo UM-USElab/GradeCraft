3c53a858ce0a822e2f452bfac014855d22e27b71
Cait Holman <caitlin.holman@gmail.com>
2012-10-05 21:52:21 -0400

    Fixed teacher assignment submission creation forms

--- app/models/user_assignment_type_weight.rb
+++ app/models/user_assignment_type_weight.rb
@@ -5,4 +5,5 @@ class UserAssignmentTypeWeight < ActiveRecord::Base
   belongs_to :assignment_type
   
   validates_presence_of :user_id, :assignment_type_id, :value
+  validates_inclusion_of :value, :in => 1..current_course.max_value
 end
