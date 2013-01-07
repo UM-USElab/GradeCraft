0ddeb049fae9c1ea88fd30d2eba78d5dd01081be
Caitlin Holman <caitlin.holman@gmail.com>
2013-01-06 23:50:51 -0500

    Tweaking homepage dashboard

--- app/controllers/info_controller.rb
+++ app/controllers/info_controller.rb
@@ -11,18 +11,24 @@ class InfoController < ApplicationController
     if current_user.is_gsi?
       @teams = current_user.teams
       @students = current_course.users.students
-      @top_ten_students = @students.order('course_memberships.sortable_score DESC').limit(10)
-      @bottom_ten_students = @students.order('course_memberships.sortable_score ASC').limit(10)
+      #@top_ten_students = @students.order('course_memberships.sortable_score DESC').limit(10)
+      #@bottom_ten_students = @students.order('course_memberships.sortable_score ASC').limit(10)
+      @top_ten_students = @students.limit(10)
+      @bottom_ten_students = @students.limit(10)
     elsif current_user.is_prof?
       @teams = current_course.teams.all
       @students = current_course.users.students
-      @top_ten_students = @students.order('course_memberships.sortable_score DESC').limit(10)
-      @bottom_ten_students = @students.order('course_memberships.sortable_score ASC').limit(10)
+      #@top_ten_students = @students.order('course_memberships.sortable_score DESC').limit(10)
+      #@bottom_ten_students = @students.order('course_memberships.sortable_score ASC').limit(10)
+      @top_ten_students = @students.limit(10)
+      @bottom_ten_students = @students.limit(10)
     elsif current_user.is_admin?
       @teams = current_course.teams.all
       @students = current_course.users.students
-      @top_ten_students = @students.order('course_memberships.sortable_score DESC').limit(10)
-      @bottom_ten_students = @students.order('course_memberships.sortable_score ASC').limit(10)
+      #@top_ten_students = @students.order('course_memberships.sortable_score DESC').limit(10)
+      #@bottom_ten_students = @students.order('course_memberships.sortable_score ASC').limit(10)
+      @top_ten_students = @students.limit(10)
+      @bottom_ten_students = @students.limit(10)
     end
     @badges = current_course.badges
     @user = current_user# 
