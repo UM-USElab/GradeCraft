e27fe90ff4c5b13fa641a0af07bc17e1a31ebd0c
Caitlin Holman <caitlin.holman@gmail.com>
2013-05-21 12:47:23 -0400

    Added gems for actionpack page and action caching

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -11,7 +11,7 @@ GradeCraft::Application.configure do
 
   # Show full error reports and disable caching
   config.consider_all_requests_local       = true
-  config.action_controller.perform_caching = false
+  config.action_controller.perform_caching = true
 
   ### ActionMailer Config
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
