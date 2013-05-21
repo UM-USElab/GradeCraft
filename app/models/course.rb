6edbf4d007eebc14f6edf2e4b4ce376137d44788
Caitlin Holman <caitlin.holman@gmail.com>
2013-05-21 15:34:11 -0400

    Updated Course Model conditions for Rails 4

--- app/models/course.rb
+++ app/models/course.rb
@@ -2,7 +2,7 @@ class Course < ActiveRecord::Base
   attr_accessible :badge_set_ids, :course_grade_scheme_id, :courseno, :name, :semester, :theme_id, :year, :badge_setting, :team_setting, :team_term, :user_term, :user_id, :course_id, :homepage_message, :group_setting, :user_weight_amount, :user_weight_amount_close_date, :team_roles, :section_leader_term, :group_term, :user_weight_amount_type, :has_assignment_submissions, :teams_visible, :badge_use_scope, :multiplier_default, :multiplier_term, :badges_value, :predictor_setting, :max_group_size, :min_group_size, :shared_badges, :graph_display
   
   has_many :course_memberships
-  has_many :users, :through => :course_memberships, :uniq => true
+  has_many :users, -> lambda { where uniq: true }, :through => :course_memberships
   accepts_nested_attributes_for :users
   
   has_many :assignments, :dependent => :destroy
@@ -30,35 +30,19 @@ class Course < ActiveRecord::Base
   end
   
   def team_ref
-    if team_term?
-      team_term
-    else 
-      "Team"
-    end
+    super || "Team"
   end
   
   def group_ref
-    if group_term?
-      group_term
-    else 
-      "Group"
-    end
+    super || "Group"
   end
   
   def section_leader_ref
-    if section_leader_term?
-      section_leader_term
-    else 
-      "Team Leader"
-    end
+    super || "Team Leader"
   end
   
   def multiplier_ref 
-    if multiplier_term?
-      multiplier_term
-    else
-      "Multiplier"
-    end
+    super || "Multiplier"
   end
   
   #Conditions
--- test/models/course_test.rb
+++ test/models/course_test.rb
@@ -5,6 +5,6 @@ class CourseTest < ActiveSupport::TestCase
   test "user_term returns default Player if no term set" do 
     @course = Course.new 
     assert_equal "Player", @course.user_term
-  end  
+  end
   
 end
